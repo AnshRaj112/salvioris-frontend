ba75a2b0615afc36e96b12e9050b55609911a2c3
AnshRaj112 <anshraj112@gmail.com>
2026-02-09 20:54:29 +0530

    removing few sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import HeroSection from "./components/Landing/HeroSection";
 import AboutUs from "./components/Landing/AboutUs";
-import TherapistGrid from "./components/Landing/TherapistGrid";
-import ChatInterface from "./components/Landing/ChatInterface";
+// import TherapistGrid from "./components/Landing/TherapistGrid";
+// import ChatInterface from "./components/Landing/ChatInterface";
 import TherapistRecruitment from "./components/Landing/TherapistRecruitment";
 import FaqSection from "./components/Landing/FaqSection";
 import ContactUs from "./components/Landing/ContactUs";
@@ -23,8 +23,8 @@ const Index = () => {
     <main className="min-h-screen m-0 p-0 w-full">
       <HeroSection />
       <AboutUs />
-      <TherapistGrid />
-      <ChatInterface />
+      {/* <TherapistGrid />
+      <ChatInterface /> */}
       <TherapistRecruitment />
       <FaqSection />
       <ContactUs />
